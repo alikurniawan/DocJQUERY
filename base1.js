cb92cf40ff88d2c02ea7d7518bab73c543d68f0e
alikurniawan <alikoerniawan02@gmail.com>
2018-09-24 11:50:36 +0700

    pENULISAN AWAL

--- base1.js
+++ base1.js
@@ -1,4 +1,11 @@
-//Base 1
+//CARA PENULISAN AWAL
+$(document).ready(function(){
+    $("button").click(function(){
+        $("p").hide();
+    });
+})
+
+//ISI NYA
 $(this).hide()
 $("h1").hide()
 $("#header").hide()
@@ -19,11 +26,27 @@ $(document).ready(function(){
         $(".d3").show("fast");
         $(".d4").show(5000);  //in Second
     })
+
+    $("#tampil").click(function(){
+        $(".d1").toggle();
+        $(".d2").toggle("slow");
+        $(".d3").toggle("fast");
+        $(".d4").toggle(5000);  //in Second
+    })
+
+    $("#btnl").click(function(){
+        $("#div1").fadeIn();
+        $("#div2").fadeIn("slow");
+        $("#div3").fadeIn("fast");
+        $("#div4").fadeIn(5000);  //in Second
+        
+        $("#div3").fadeTo(3000, 0.5); //(Milisecond, Opacity)
+
+        $("#div1").fadeOut();
+        $("#div2").fadeOut("slow");
+        $("#div3").fadeOut("fast");
+        $("#div4").fadeOut(5000);  //in Second
+    })
 });
 
-//Base2
-$(document).ready(function(){
-    $("button").click(function(){
-        $("p").hide();
-    });
-})
+
