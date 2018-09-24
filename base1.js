d97096a52b24b06552753ffdb9004c70d620a768
alikurniawan <alikoerniawan02@gmail.com>
2018-09-24 10:27:53 +0700

    hide and show

--- base1.js
+++ base1.js
@@ -1,4 +1,29 @@
+//Base 1
 $(this).hide()
 $("h1").hide()
 $("#header").hide()
 $(".posting").hide()
+
+//Base 1.1
+$(document).ready(function(){
+    $("#sembunyi").click(function(){
+        $(".d1").hide();
+        $(".d2").hide("slow");
+        $(".d3").hide("fast");
+        $(".d4").hide(5000);  //in Second
+    })
+
+    $("#tampil").click(function(){
+        $(".d1").show();
+        $(".d2").show("slow");
+        $(".d3").show("fast");
+        $(".d4").show(5000);  //in Second
+    })
+});
+
+//Base2
+$(document).ready(function(){
+    $("button").click(function(){
+        $("p").hide();
+    });
+})
