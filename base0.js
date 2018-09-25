9d953a06b7e999f167fa57eae45548277fd6a274
alikurniawan <alikoerniawan02@gmail.com>
2018-09-26 01:15:19 +0700

    base0

--- base0.js
+++ base0.js
@@ -24,15 +24,35 @@ $(document).ready(function(){
         $("tr:odd").css("background-color", "aqua");  //CSS Script
     })
 
-    $("[alt='link1]").click(function(){
+    $("[alt='link1']").click(function(){
         $("[href]").css("color", "red");  //<a href="#">paragraph pertama first dari parent</a>
     })
 
-    $("[alt='link2]").click(function(){
+    $("[alt='link2']").click(function(){
         $("a[target='_blank']").css("color", "red");  //<a href="#">paragraph pertama first dari parent</a>
     })
 
     $("img[width]").click(function(){
         $("a[target!='_blank']").css("border", "4px solid #000");  //<a href="#">paragraph pertama first dari parent</a>
     })
+
+    $("[alt='sembunyi_tombol']").click(function(){
+        $(":button").slideUp(4000);  // 4000 detik --> :button maksudnya menyeleksi button
+    })
+
+    $("#judul3").on({
+        mouseenter:function(){
+            $(this).css("background-color", "black");
+        },
+
+        mouseleave:function(){
+            $(this).css("background-color", "blue");
+        },
+
+        click:function(){
+            $(this).css("background-color", "black");
+        }
+
+
+    });
 })
