5046eb6f9c4ade1b18254398b6bc70a13cd76ec8
alikurniawan <alikoerniawan02@gmail.com>
2018-09-26 14:02:15 +0700

    animateJQuery

--- animate.js
+++ animate.js
@@ -2,4 +2,13 @@ $(document).ready(function(){
     $(".animasi").click(function(){   //.animasi adalah class button
         $(".isi").animate({left: '500px'});
     })
+
+    $(".animasi2").click(function(){
+        $(".isi2").animate({
+            left : '500px',
+            top : '100px',
+            fontSize : '2cm',
+            padding : '40px'
+        }, 5000, "linear");
+    })
 })
