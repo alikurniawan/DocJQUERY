f841cdb9e9128e0d807a1ef39c58ede566229f05
alikurniawan <alikoerniawan02@gmail.com>
2018-09-26 16:13:12 +0700

    animateStop

---
+++ animateStop.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    $("#mulai").click(function(){
+        $("#efek1").slideUp(5000);
+        $("#efek2").slideDown(5000);
+        $("#efek3").animate({left:'400px'}, 5000);
+        $("#efek4").hide(5000);
+        $("#efek5").show(5000);
+    })
+
+    //STOP ANIMASI SEBAGIAN
+    $("#berhenti1").click(function(){
+        $("#efek1").stop();
+        $("#efek2").stop();
+        $("#efek3").stop();
+    })
+
+    //STOP SELURUHNYA
+    $("#berhenti1").click(function(){
+        $("#efek1").stop(true);
+        $("#efek2").stop(true);
+        $("#efek3").stop(true);
+    })
+})
