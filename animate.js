ec139e632759421c81464021049ee51d0de63cec
alikurniawan <alikoerniawan02@gmail.com>
2018-09-26 14:27:20 +0700

    animateJQuery

--- animate.js
+++ animate.js
@@ -11,4 +11,36 @@ $(document).ready(function(){
             padding : '40px'
         }, 5000, "linear");
     })
+
+    $(".animasi3").click(function(){
+        $(".isi3").animate({
+            left : '+=500px',  //Setiap tombol button di click akan bertambah 500px
+            top : '+=100px',
+        }, "slow");
+    })
+
+    $(".animasi4").click(function(){
+        $(".isi4").animate({
+            left : 'toggle',  
+            top : 'toggle',
+        }, "fast", "swing");
+    })
+
+    $(".animasi4").click(function(){
+        $(".isi5, .isi4").animate({
+            top: '100px',
+            left: '60px',
+            fintSize: '2cm'
+        });
+        $(".isi5, .isi2").animate({
+            top: '100px',
+            left: '60px',
+            fintSize: '2cm'
+        });
+        $(".isi5, .isi1").animate({
+            top: '100px',
+            left: '60px',
+            fintSize: '2cm'
+        });
+    })
 })
