6cc81419354319363c6082f25e3da1a72c38aaf7
alikurniawan <alikoerniawan02@gmail.com>
2018-09-26 20:51:48 +0700

    callbackHitung

--- callbackAlertHitung.js
+++ callbackAlertHitung.js
@@ -16,12 +16,24 @@ $(document).ready(function(){
         alert("selesai");
     }
 
+    function hitung1(panjang, lebar){
+        var p = panjang;
+        var l = lebar;
+        var luas = p * l;
+
+        document.getElementById("d2").innerHTML +=
+        'Panjang = ' + p + '<br>' + 
+        'Lebar = ' + l + '<br>' +
+        'Luas = ' + luas;
+    }
+
     $(".b2").click(function(){
         $("#d2").animate({
             width: '400px',
             fontSize: '30px'
         }, "slow", function(){
             tampil();
+            hitung1(20, 50);
         })
     })
 
