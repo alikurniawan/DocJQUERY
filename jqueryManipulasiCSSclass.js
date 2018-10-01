1daa38ceb0ffa9e62a104b138ffb8bfe8534ef8f
alikurniawan <alikoerniawan02@gmail.com>
2018-10-01 15:10:54 +0700

    addClass

---
+++ jqueryManipulasiCSSclass.js
@@ -0,0 +1,31 @@
+//HTML
+{/* <body>
+    <button id="kelas1">Tombol 1</button>
+    <button id="kelas2">Tombol 2</button>
+    <button id="kelas3">Tombol 3</button>
+
+    <h3>Header</h3>
+    <p>Paragraf</p>
+    <p>Paragrah</p>
+</body>
+
+//CSS
+<style>
+    .huruf{
+        font-family: arial;
+        color: blue;
+    }
+
+    .header1{
+        font-size: 40px;
+        border: 2px dotted #999;
+    }
+</style> */}
+
+//JQUERY EXECUTION
+$(document).ready(function(){
+    $("#kelas1").click(function(){
+        $("h3, p").addClass("huruf"); //---> diarahkan atau ( memasukkan class ) ke class CSS dengan nama huruf
+        $("h3").addClass("header1"); //--> 
+    })
+})
